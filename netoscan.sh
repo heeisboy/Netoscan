9c019ded67ca23a0ccca8d788a1796bfefeb3f4f
heeisboy <worldwadeway@gmail.com>
2026-04-05 17:26:57 +0300

    добавил trap и проверку на root

--- netoscan.sh
+++ netoscan.sh
@@ -3,6 +3,13 @@
 PREFIX="${1:-NOT_SET}"
 INTERFACE="$2"
 
+trap "echo '--- Ctrl+C pressed, exiting ---'; exit 1" 2 # При получении сигнала прерывания выходим из скрипта
+
+if [[ $EUID -ne 0 ]]; then  # Проверяем, запущен ли скрипт от имени root, если не запущен, выводим предупреждение и выходим из скрипта
+	echo "Error: run as root (sudo $0 ...) !" >&2
+    exit 1
+fi
+
 [[ "$PREFIX" = "NOT_SET" ]] && { echo "\$PREFIX must be passed as first positional argument"; exit 1; }
 if [[ -z "$INTERFACE" ]]; then
     echo "\$INTERFACE must be passed as second positional argument"
